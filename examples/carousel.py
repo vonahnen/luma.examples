4060cd3157c23cf544ce9e6f2b0c0389ec2aa4c6
vonahnen <kristofer.vonahnen@gmail.com>
2024-04-06 15:10:09 -0500

    Working on customizing carousel.py.

--- examples/carousel.py
+++ examples/carousel.py
@@ -19,6 +19,9 @@ import psutil
 
 from demo_opts import get_device
 from luma.core.virtual import viewport, snapshot
+from time import sleep
+from datetime import datetime
+from math import ceil
 
 from hotspot import memory, uptime, cpu_load, clock, network, disk
 
@@ -32,13 +35,16 @@ def position(max):
         for x in backwards:
             yield x
 
+def runtime_seconds(start_utc):
+    return (datetime.utcnow() - start_utc).total_seconds()
 
-def pause_every(interval, generator):
+def pause_every(freeze_display_seconds, interval, generator):
     try:
         while True:
             x = next(generator)
             if x % interval == 0:
-                for _ in range(20):
+                start_utc = datetime.utcnow()
+                while runtime_seconds(start_utc) < freeze_display_seconds:
                     yield x
             else:
                 yield x
@@ -60,48 +66,45 @@ def first(iterable, default=None):
 def main():
     if device.rotate in (0, 2):
         # Horizontal
-        widget_width = device.width // 2
+        full_widget_width = device.width
+        half_widget_width = device.width // 2
         widget_height = device.height
     else:
         # Vertical
         widget_width = device.width
         widget_height = device.height // 2
 
-    # Either function or subclass
-    #  cpuload = hotspot(widget_width, widget_height, cpu_load.render)
-    #  cpuload = cpu_load.CPU_Load(widget_width, widget_height, interval=1.0)
-    utime = snapshot(widget_width, widget_height, uptime.render, interval=1.0)
-    mem = snapshot(widget_width, widget_height, memory.render, interval=2.0)
-    dsk = snapshot(widget_width, widget_height, disk.render, interval=2.0)
-    cpuload = snapshot(widget_width, widget_height, cpu_load.render, interval=0.5)
-    clk = snapshot(widget_width, widget_height, clock.render, interval=1.0)
+    cpuload = snapshot(full_widget_width, widget_height, cpu_load.render, interval=0.33)
+    mem = snapshot(full_widget_width, widget_height, memory.render, interval=0.33)
+    dsk = snapshot(full_widget_width, widget_height, disk.render, interval=1.0)
+    eth = first(intersect(psutil.net_if_stats().keys(), ["eth0", "en0"]), "eth0")
+    net_eth = snapshot(full_widget_width, widget_height, network.stats(eth), interval=0.5)
 
-    network_ifs = psutil.net_if_stats().keys()
-    wlan = first(intersect(network_ifs, ["wlan0", "wl0"]), "wlan0")
-    eth = first(intersect(network_ifs, ["eth0", "en0"]), "eth0")
-    lo = first(intersect(network_ifs, ["lo", "lo0"]), "lo")
+    full_widgets = [cpuload, mem, dsk, net_eth]
+    half_widgets = []
 
-    net_wlan = snapshot(widget_width, widget_height, network.stats(wlan), interval=2.0)
-    net_eth = snapshot(widget_width, widget_height, network.stats(eth), interval=2.0)
-    net_lo = snapshot(widget_width, widget_height, network.stats(lo), interval=2.0)
-
-    widgets = [cpuload, utime, clk, net_wlan, net_eth, net_lo, mem, dsk]
+    freeze_display_seconds = 10
 
     if device.rotate in (0, 2):
-        virtual = viewport(device, width=widget_width * len(widgets), height=widget_height)
-        for i, widget in enumerate(widgets):
-            virtual.add_hotspot(widget, (i * widget_width, 0))
+        viewport_width = len(full_widgets) * full_widget_width + len(half_widgets) * half_widget_width
+        virtual = viewport(device, width=viewport_width, height=widget_height)
 
-        for x in pause_every(widget_width, position(widget_width * (len(widgets) - 2))):
+        for i, widget in enumerate(full_widgets):
+            virtual.add_hotspot(widget, (i * full_widget_width, 0))
+
+        for i, widget in enumerate(half_widgets):
+            virtual.add_hotspot(widget, (len(full_widgets) * full_widget_width + i * half_widget_width, 0))
+
+        for x in pause_every(freeze_display_seconds, device.width, position((len(full_widgets) * full_widget_width + len(half_widgets) * half_widget_width - device.width))):
             virtual.set_position((x, 0))
 
-    else:
-        virtual = viewport(device, width=widget_width, height=widget_height * len(widgets))
-        for i, widget in enumerate(widgets):
-            virtual.add_hotspot(widget, (0, i * widget_height))
+    # else:
+    #     virtual = viewport(device, width=widget_width, height=widget_height * len(widgets))
+    #     for i, widget in enumerate(widgets):
+    #         virtual.add_hotspot(widget, (0, i * widget_height))
 
-        for y in pause_every(widget_height, position(widget_height * (len(widgets) - 2))):
-            virtual.set_position((0, y))
+    #     for y in pause_every(freeze_display_seconds, widget_height, position(widget_height * (len(widgets) - 2))):
+    #         virtual.set_position((0, y))
 
 
 if __name__ == "__main__":
